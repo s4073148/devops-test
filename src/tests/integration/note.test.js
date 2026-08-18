b9bfdcebe0fd0185049353932ed1386a867611b9
s4073148 <s4073148.student.rmit.edu.au>
2026-08-18 14:47:03 +1000

    test11

--- src/tests/integration/note.test.js
+++ src/tests/integration/note.test.js
@@ -1,7 +1,7 @@
 const app = require('../../app')
 const supertest = require('supertest')
 const req = supertest(app)
-const Note = require("../../models/note")
+// const Note = require("../../models/note")
 
 // Integration Tests programmatically test the API used by the application
 
