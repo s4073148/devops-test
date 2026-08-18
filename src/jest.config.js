a6df037b1f84a72a93416cf0aac051250da2f4d6
s4073148 <s4073148.student.rmit.edu.au>
2026-08-18 15:21:38 +1000

    test16

--- src/jest.config.js
+++ src/jest.config.js
@@ -24,7 +24,7 @@ module.exports = {
   collectCoverageFrom: ["models/*.js"],
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage/",
+  coverageDirectory: "logs/coverage/",
 
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
